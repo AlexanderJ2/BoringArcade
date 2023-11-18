eba88a341e4072be9777aa70fc85c58775b2bbc3
DismissedGv <129997550+DismissedGv@users.noreply.github.com>
2023-11-18 17:45:22 +0100

    player movement finished

---
+++ GameJamMashup/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Money
+    public int money = 0;
+
+    //Movement
+    public CharacterController controller;
+    public Transform cam;
+    [SerializeField] float speed = 6f;
+    float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, Vertical);
+    
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; //Moves with camera angle
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
